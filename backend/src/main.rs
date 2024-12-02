3ab555aff20e9bb7fa0391b2fcdc98c2987d26f5
aymanhki <agamyahk@myumanitoba.ca>
2024-12-01 21:28:47 -0600

    Fixed fargate deployment to action workflow #8.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,7 +8,7 @@ mod repository;
 use actix_web::{App, HttpServer, web};
 use actix_cors::Cors;
 use crate::config::Config;
-use crate::db::db_connection::init_db_connection;
+// use crate::db::db_connection::init_db_connection;
 use crate::routes::init_routes;
 
 
@@ -20,7 +20,7 @@ async fn main() -> std::io::Result<()> {
 
 async fn start_server() -> std::io::Result<()> {
     let config = Config::load_from_env().expect("Failed to load configuration");
-    let db_pool = init_db_connection(&config.database_url).await.expect("Failed to create database connection pool");
+    // let db_pool = init_db_connection(&config.database_url).await.expect("Failed to create database connection pool");
 
     let server = HttpServer::new(move || {
         App::new()
@@ -32,7 +32,7 @@ async fn start_server() -> std::io::Result<()> {
                     .allowed_headers(vec!["Content-Type", "Authorization"])
                     .supports_credentials()
             )
-            .app_data(web::Data::new(db_pool.clone()))
+            // .app_data(web::Data::new(db_pool.clone()))
             .configure(init_routes)
     })
         .bind(&config.server_address)?;
